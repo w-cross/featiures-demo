582732bcf266d02b03848ca47f5b3c8380f09a1f
apple <wzw2984521645@163.com>
2019-12-11 23:00:55 +0800

    提交java新特性commit

--- src/main/java/com/example/newfeature/features/Features10.java
+++ src/main/java/com/example/newfeature/features/Features10.java
@@ -1,5 +1,6 @@
 package com.example.newfeature.features;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Features10 {
@@ -8,6 +9,16 @@ public class Features10 {
         var x1 = new ArrayList<String>();
         var x2 = "abs";
         System.out.println(x2);
+
+        try {
+            var fileInputStream = new FileInputStream("/Users/apple/Desktop/in.txt");
+            var outPutStream = new FileOutputStream("/Users/apple/Desktop/out.txt");
+            fileInputStream.transferTo(outPutStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
